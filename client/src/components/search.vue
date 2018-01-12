da32bd569cdab9e4599060772c795ce3994e4397
Yiannis <ytsampouris@terracognita.gr>
2018-01-12 16:29:40 +0200

    explore tab

--- client/src/components/search.vue
+++ client/src/components/search.vue
@@ -1,43 +1,132 @@
 <template>
-  <timeline></timeline>
-</v-container>
+  <div>
+    <v-card color="grey lighten-4" flat height="200px">
+      <v-toolbar color="white" light dense>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Συλλογές</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>favorite</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-container fluid grid-list-md class="grey lighten-4">
+        <v-layout row wrap>
+          <v-flex
+            v-bind="{ [`xs${card.flex}`]: true }"
+            v-for="card in cards"
+            :key="card.title"
+          >
+            <v-card>
+              <v-card-media
+                :src="card.src"
+                height="0px"
+              >
+                <v-container fill-height fluid>
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox>
+                      <span class="headline white--text" v-text="card.title"></span>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card-media>
+              <v-card-actions class="white">
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>bookmark</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>share</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>edit</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>delete</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
 
+
+
+        <v-card color="grey lighten-4" flat height="200px">
+      <v-toolbar color="white" light dense>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-toolbar-title>Ομάδες</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>favorite</v-icon>
+        </v-btn>
+        <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-container fluid grid-list-md class="grey lighten-4">
+        <v-layout row wrap>
+          <v-flex
+            v-bind="{ [`xs${card.flex}`]: true }"
+            v-for="card in cards"
+            :key="card.title"
+          >
+            <v-card>
+              <v-card-media
+                height="0px"
+              >
+                <v-container fill-height fluid>
+                  <v-layout fill-height>
+                    <v-flex xs12 align-end flexbox>
+                      <span class="headline white--text" v-text="card.title"></span>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card-media>
+              <v-card-actions class="white">
+                <v-spacer></v-spacer>
+                <v-btn icon>
+                  <v-icon>bookmark</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>share</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>edit</v-icon>
+                </v-btn>
+                <v-btn icon>
+                  <v-icon>delete</v-icon>
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-card>
+  </div>
 </template>
 <script>
-import timeline from './timeline';
-
 export default {
   name: 'search',
   data() {
     return {
-      ex1: [],
-      ex2: [],
-      amenities: [],
-      e3: null,
-      e4: null,
-      e2: 3,
-      modal: false,
-      chip2: true,
-      mode: 0,
+      cards: [
+        { title: 'Pre-fab homes', flex: 6 },
+        { title: 'Favorite road trips', flex: 6 },
+        { title: 'Best airlines', flex: 6 },
+      ],
     };
   },
   components: {
-    timeline,
   },
 };
 </script>
-<style>
-.fa-bed {
-  padding: 2px;
-  align-items: center;
-  align: center;
-}
-.flex-beds {
-  align-items: center;
-  align-content: center;
-  align-self: center;
-}
-.query-option {
-  padding: 0px;
-}
-</style>
