15ff80a289fc50ec34e45a5a1c90094eeb36f48b
Γιάννης Τσαμπούρης < >
2018-01-14 20:23:27 +0200

    collections gui

--- client/src/components/search.vue
+++ client/src/components/search.vue
@@ -1,132 +1,103 @@
 <template>
-  <div>
-    <v-card color="grey lighten-4" flat height="200px">
-      <v-toolbar color="white" light dense>
-        <v-toolbar-side-icon></v-toolbar-side-icon>
-        <v-toolbar-title>Συλλογές</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>search</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>favorite</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>more_vert</v-icon>
-        </v-btn>
-      </v-toolbar>
-      <v-container fluid grid-list-md class="grey lighten-4">
-        <v-layout row wrap>
-          <v-flex
-            v-bind="{ [`xs${card.flex}`]: true }"
-            v-for="card in cards"
-            :key="card.title"
-          >
-            <v-card>
-              <v-card-media
-                :src="card.src"
-                height="0px"
-              >
-                <v-container fill-height fluid>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <span class="headline white--text" v-text="card.title"></span>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card-media>
-              <v-card-actions class="white">
-                <v-spacer></v-spacer>
-                <v-btn icon>
-                  <v-icon>bookmark</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon>share</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon>edit</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon>delete</v-icon>
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-card>
-
-
-
-        <v-card color="grey lighten-4" flat height="200px">
-      <v-toolbar color="white" light dense>
-        <v-toolbar-side-icon></v-toolbar-side-icon>
-        <v-toolbar-title>Ομάδες</v-toolbar-title>
-        <v-spacer></v-spacer>
-        <v-btn icon>
-          <v-icon>search</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>favorite</v-icon>
-        </v-btn>
-        <v-btn icon>
-          <v-icon>more_vert</v-icon>
-        </v-btn>
-      </v-toolbar>
-      <v-container fluid grid-list-md class="grey lighten-4">
-        <v-layout row wrap>
-          <v-flex
-            v-bind="{ [`xs${card.flex}`]: true }"
-            v-for="card in cards"
-            :key="card.title"
-          >
-            <v-card>
-              <v-card-media
-                height="0px"
-              >
-                <v-container fill-height fluid>
-                  <v-layout fill-height>
-                    <v-flex xs12 align-end flexbox>
-                      <span class="headline white--text" v-text="card.title"></span>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card-media>
-              <v-card-actions class="white">
-                <v-spacer></v-spacer>
-                <v-btn icon>
-                  <v-icon>bookmark</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon>share</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon>edit</v-icon>
-                </v-btn>
-                <v-btn icon>
-                  <v-icon>delete</v-icon>
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-card>
-  </div>
+  <v-layout row>
+    <v-flex xs12 sm12>
+      <v-card>
+        <v-toolbar color="orange" dark>
+          <v-toolbar-side-icon></v-toolbar-side-icon>
+          <v-toolbar-title>Συλλογές</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon>
+            <v-icon>search</v-icon>
+          </v-btn>
+          <v-btn icon>
+            <v-icon>add</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-list two-line subheader>
+          <v-subheader inset>Προσωπικές Συλλογές</v-subheader>
+          <v-list-tile avatar v-for="item in privateCollections" v-bind:key="item.title">
+            <v-list-tile-content>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ item.description }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn icon ripple>
+                <v-icon color="grey lighten-1">info</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+        <v-list two-line subheader>
+          <v-subheader inset>Δημόσιες Συλλογές</v-subheader>
+          <v-list-tile avatar v-for="item in publicCollections" v-bind:key="item.title">
+            <v-list-tile-content style='overflow-y: auto;'>
+              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+              <v-list-tile-sub-title>{{ item.description }}</v-list-tile-sub-title>
+            </v-list-tile-content>
+            <v-list-tile-action>
+              <v-btn icon ripple>
+                <v-icon color="grey lighten-1">info</v-icon>
+              </v-btn>
+            </v-list-tile-action>
+          </v-list-tile>
+        </v-list>
+      </v-card>
+    </v-flex>
+  </v-layout>
 </template>
 <script>
+import axios from 'axios';
+import config from '../config';
+
 export default {
   name: 'search',
   data() {
     return {
-      cards: [
-        { title: 'Pre-fab homes', flex: 6 },
-        { title: 'Favorite road trips', flex: 6 },
-        { title: 'Best airlines', flex: 6 },
+      privateCollections: [
+        { title: 'kozani test1', description: 'test', flex: 6, id: 'jfjkdlaiefjie37492jfiosf98w' },
+        { title: 'kozani test2', description: 'test', flex: 6, id: 'jfjkdlaiefjie37492jfiosf98w' },
+      ],
+      publicCollections: [
+        { title: 'kozani test public1', description: 'test', flex: 6, id: 'jfjkdlaiefjie37492jfiosf98w' },
+        { title: 'kozani test public2', description: 'test', flex: 6, id: 'jfjkdlaiefjie37492jfiosf98w' },
+        { title: 'kozani test public3', description: 'test', flex: 6, id: 'jfjkdlaiefjie37492jfiosf98w' },
+        { title: 'kozani test public1', description: 'test', flex: 6, id: 'jfjkdlaiefjie37492jfiosf98w' },
+        { title: 'kozani test public2', description: 'test', flex: 6, id: 'jfjkdlaiefjie37492jfiosf98w' },
+        { title: 'kozani test public3', description: 'test', flex: 6, id: 'jfjkdlaiefjie37492jfiosf98w' },
+        { title: 'kozani test public1', description: 'test', flex: 6, id: 'jfjkdlaiefjie37492jfiosf98w' },
+        { title: 'kozani test public2', description: 'test', flex: 6, id: 'jfjkdlaiefjie37492jfiosf98w' },
+        { title: 'kozani test public3', description: 'test', flex: 6, id: 'jfjkdlaiefjie37492jfiosf98w' },
+      ],
+      groups: [
+        { title: 'group1', flex: 6, id: 'jfkd33jf88nvn884ng8n26nr6', members: [] },
       ],
     };
   },
   components: {
+  },
+  methods: {
+    getPublicCollections() {
+      const serverUrl = `${config.APIhttpType}://${config.APIhost}:${config.APIhostPort}/${config.APIversion}/collections`;
+      axios.get(serverUrl, { params: {
+        type: 'public',
+      },
+      }).then((response) => {
+        this.publicCollections = response.data;
+      }).then(() => {
+        this.loading = false;
+      });
+    },
+    getPrivateCollections() {
+      const serverUrl = `${config.APIhttpType}://${config.APIhost}:${config.APIhostPort}/${config.APIversion}/collections`;
+      axios.get(serverUrl, { params: {
+        type: 'private',
+      },
+      }).then((response) => {
+        this.privateCollections = response.data;
+      }).then(() => {
+        this.loading = false;
+      });
+    },
   },
 };
 </script>
