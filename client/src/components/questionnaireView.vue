d7f433cb80815be89220963bf82f6c364b4585c8
Γιάννης Τσαμπούρης < >
2019-02-24 20:51:37 +0200

    checking if all questions are filled

--- client/src/components/questionnaireView.vue
+++ client/src/components/questionnaireView.vue
@@ -1,7 +1,9 @@
 <template>
   <v-layout row wrap xs12 sm12 md12>
-      
-      <v-container fluid row v-for="item in questionnaire.properties.introduction.items" :key="item.id">
+      <v-container fluid row
+        v-for="item in questionnaire.properties.introduction.items"
+        :key="item.id"
+      >
         <h3 v-if="item.type === 'heading'" class="headline mb-0">{{ item.value }}</h3>
         <div v-if="item.type === 'text'" > {{ item.value }} </div>
         <img v-if="item.type === 'image'" :src="item.value" aspect-ratio="2.75"/>
@@ -52,7 +54,7 @@
           </v-container>
 
           <v-container row wrap v-if="question.type === 'radioGroup'">
-            <v-radio-group v-model="question.value">
+            <v-radio-group v-model="question.value" mandatory>
               <v-radio
                 v-for="radio in question.radios"
                 :key="radio.id"
@@ -61,7 +63,7 @@
               ></v-radio>
             </v-radio-group>
           </v-container>
-          
+
           <v-container row wrap v-if="question.type === 'mapPointer'">
             <v-flex v-for="button in question.buttons" :key="button.id">{{ button.label }}
               <v-btn small fab dark class="indigo" @click="getFromMap(button.id, 'Point')">
@@ -93,7 +95,10 @@
           </v-card-text>
         </v-card>
       </v-container>
-      <v-btn dark class="indigo" @click="getValues">
+      <v-btn dark class="indigo" @click="page += 1;">
+        Επόμενη Ενότητα >>
+      </v-btn>
+      <v-btn dark class="indigo" @click="submit()">
         ΥΠΟΒΟΛΗ ΕΡΩΤΗΜΑΤΟΛΟΓΙΟΥ<v-icon dark>send</v-icon>
       </v-btn>
   </v-layout>
@@ -105,6 +110,7 @@ import olMap from '../js/map';
 export default {
   data() {
     return {
+      page: 0,
       questionnaire: {
         properties: {
           mongoid: 'fjdkslajfsdkgjeio111',
@@ -112,10 +118,10 @@ export default {
           loginRequired: false,
           introduction: {
             items: [
-              { id: 1, type: 'heading', value: 'Εντοπισμός προβλημάτων' },
-              { id: 2, type: 'text', value: 'περιγραφή του ερωτηματολογίου' },
+              { id: 1, type: 'heading', value: 'Ερωτηματολόγιο για εντοπισμό προβλημάτων' },
+              { id: 2, type: 'text', value: 'Αυτό το ερωτηματολόγιο έχει σκοπό να συλλέξει τα προβλήματα που εμφανίζονται στις μετακινήσεις των πολιτών στην πόλη των Τρικάλων' },
               { id: 3, type: 'image', value: 'https://cdn.vuetifyjs.com/images/cards/desert.jpg' },
-              { id: 4, type: 'text', value: 'περιγραφή της εικόνας' },
+              { id: 4, type: 'text', value: 'Συμπληρώστε το ερωτηματολόγιο σύμφωνα με τις οδηγίες κάθε ερώτησης. Στις ερωτήσεις που χρειάζεται να δείξετε πάνω στον χάρτη πατήστε το μπλε κουμπί και μετά πάνω στο χάρτη. Αν θέλετε να διορθώσετε τη θέση που δείξατε επαναλάβετε τη διαδικασία' },
             ],
           },
         },
@@ -130,7 +136,7 @@ export default {
           },
           { id: 'question2',
             type: 'combobox',
-            page: '2',
+            page: '1',
             title: 'Πόσα αυτοκίνητα διαθέτει το νοικοκυριό σας;',
             description: 'Διαλέξτε από τη λίστα',
             value: null,
@@ -138,7 +144,7 @@ export default {
           },
           { id: 'question3',
             type: 'checkboxGroup',
-            page: '2',
+            page: '1',
             title: 'Πότε μετακινήστε προς το κέντρο της πόλης;',
             description: 'Διαλέξτε ένα ή περισσότερα από τα παρακάτω',
             value: null,
@@ -149,12 +155,12 @@ export default {
           },
           { id: 'question4',
             type: 'radioGroup',
-            page: '2',
+            page: '1',
             title: 'Ποιον τρόπο μετακίνησης προτιμάτε;',
             description: 'Διαλέξτε ένα από τα παρακάτω',
             value: null,
             radios: [
-              { id: 'r1', label: 'Αυτοκίνητο', radioValue: false },
+              { id: 'r1', label: 'Αυτοκίνητο', radioValue: true },
               { id: 'r2', label: 'Λεωφορείο', radioValue: false },
               { id: 'r3', label: 'Πεζή', radioValue: false },
               { id: 'r4', label: 'Ποδήλατο', radioValue: false },
@@ -194,46 +200,101 @@ export default {
     };
   },
   methods: {
+    async validate(questionnaire) {
+      let error = false;
+      questionnaire.forEach((q) => {
+        if (q.error) {
+          console.log('there is error in ::: ', q.id);
+          error = true;
+        }
+      });
+      console.log(!error);
+      return !error;
+    },
     addRow(question) {
       if (question.lines.length < 21) {
-        question.lines.push({ id: `question.id${question.lines.length + 1}`, value: null, coords: [] });
+        question.lines.push({ id: `${question.id}${question.lines.length + 1}`, value: null, coords: [] });
       }
-      console.log(`question.id${question.lines.length + 1}`);
+      console.log(`added this id:: ${question.id}${question.lines.length + 1}`);
     },
-    getValues() {
+    async getValues() {
       const questionnaireResult = [];
       this.questionnaire.questions.forEach((q) => {
         if (q.type === 'textfield') {
-          questionnaireResult.push({
-            id: q.id,
-            title: q.title,
-            value: q.value,
-          });
+          if (q.value && q.value.length > 0) {
+            questionnaireResult.push({
+              id: q.id,
+              title: q.title,
+              value: q.value,
+              error: false,
+            });
+          } else {
+            questionnaireResult.push({
+              id: q.id,
+              title: q.title,
+              value: q.value,
+              error: true,
+            });
+          }
         }
         if (q.type === 'combobox') {
-          questionnaireResult.push({
-            id: q.id,
-            title: q.title,
-            value: q.value,
-          });
+          if (q.value && q.value.length > 0) {
+            questionnaireResult.push({
+              id: q.id,
+              title: q.title,
+              value: q.value,
+              error: false,
+            });
+          } else {
+            questionnaireResult.push({
+              id: q.id,
+              title: q.title,
+              value: q.value,
+              error: true,
+            });
+          }
         }
         if (q.type === 'checkboxGroup') {
           const boxes = [];
+          const errors = [];
+          q.checkboxes.forEach((c) => {
+            errors.push(c.value);
+          });
           q.checkboxes.forEach((c) => {
             boxes.push([c.label, c.value]);
           });
-          questionnaireResult.push({
-            id: q.id,
-            title: q.title,
-            value: boxes,
-          });
+          if (errors.includes(true)) {
+            questionnaireResult.push({
+              id: q.id,
+              title: q.title,
+              value: boxes,
+              error: false,
+            });
+          } else {
+            questionnaireResult.push({
+              id: q.id,
+              title: q.title,
+              value: boxes,
+              error: true,
+            });
+          }
         }
         if (q.type === 'radioGroup') {
-          questionnaireResult.push({
-            id: q.id,
-            title: q.title,
-            value: q.value,
-          });
+          if (q.value) {
+            questionnaireResult.push({
+              id: q.id,
+              title: q.title,
+              value: q.value,
+              error: false,
+            });
+          } else {
+            questionnaireResult.push({
+              id: q.id,
+              title: q.title,
+              value: q.value,
+              error: true,
+            });
+          }
         }
         if (q.type === 'mapPointer') {
           let coordinates = null;
@@ -249,27 +310,46 @@ export default {
             id: q.id,
             title: q.title,
             value: coordinates,
+            error: false,
           });
         }
         if (q.type === 'mapPointerMultiple') {
           const coordinates = [];
+          const values = [];
           q.lines.forEach((b) => {
-            const features = this.$store.state.questionnaireFeatures;
-            features.forEach((f) => {
-              if (f.getProperties().buttonId === b.id) {
-                coordinates.push(f);
-              }
+            console.log('b:: ', b);
+            if (b.value) {
+              values.push(b.value);
+              const features = this.$store.state.questionnaireFeatures;
+              features.forEach((f) => {
+                if (f.getProperties().buttonId === b.id) {
+                  coordinates.push(f);
+                }
+              });
+            }
+          });
+          if (values.length > 0) {
+            questionnaireResult.push({
+              id: q.id,
+              title: q.title,
+              value: values,
+              coordinates: coordinates,
+              error: false,
             });
-          });
-          questionnaireResult.push({
-            id: q.id,
-            title: q.title,
-            value: coordinates,
-          });
+          } else {
+            questionnaireResult.push({
+              id: q.id,
+              title: q.title,
+              value: values,
+              coordinates: coordinates,
+              error: true,
+            });
+          }
           console.log(q.id, q.title, q.value);
         }
       });
       console.log('result:: ', questionnaireResult);
+      return questionnaireResult;
     },
     getFromMap(id, type) {
       let allLayers = [];
@@ -286,23 +366,24 @@ export default {
 
       this.$store.commit('setQuestionnaireFeatureId', id);
       olMap.getInteractions().forEach((interaction) => {
-        if (type === 'Point') {
-          this.toolColors = ['green', 'grey', 'grey', 'grey', 'grey'];
-        }
-        if (type === 'LineString') {
-          this.toolColors = ['grey', 'green', 'grey', 'grey', 'grey'];
-        }
-        if (type === 'Polygon') {
-          this.toolColors = ['grey', 'grey', 'green', 'grey', 'grey'];
-        }
         if (interaction instanceof ol.interaction.Draw) {
           if (interaction.getProperties().name === type) {
             interaction.setActive(true);
-            // this.selectedTool = 'drawFeatures';
           } else {
             interaction.setActive(false);
           }
         }
+      });
+    },
+    submit() {
+      this.getValues().then((res) => {
+        this.validate(res).then((v) => {
+          if (v) {
+            console.log('OK so submit', v);
+          } else {
+            console.log('show the error', v);
+          }
+        });
       });
     },
   },
